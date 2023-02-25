3d8500d6cd1379df3b39a82541ef7e4764c892f7
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-02-25 14:49:49 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+    <a href="../page1"><h1>Page 1</h1></a>
+    <a href="../page2"><h1>Page 2</h1></a>
+</div>
---
+++ src/routes/page1/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<div>
+    <h1>Welcome to Page 1</h1>
+    <div>
+        <a href="../"><h1>Home</h1></a>
+        <a href="../page2"><h1>Page 2</h1></a>
+    </div>
+</div>
